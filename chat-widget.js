c834d2a3b5a964dfb3c294820336b57f450d7aaa
zachsupwork <zachsupwork@gmail.com>
2025-02-06 00:48:13 -0500

    Update chat-widget.js

--- chat-widget.js
+++ chat-widget.js
@@ -11,18 +11,6 @@
       chatWidgetScript.src = "https://storage.googleapis.com/cdwidget/dist/assets/js/main.js";
       document.body.appendChild(chatWidgetScript);
 
-      chatWidgetScript.onload = function() {
-          var checkExist = setInterval(function() {
-              var widgetIcon = document.querySelector('#cd-widget img'); // Adjust the selector if needed
-              if (widgetIcon) {
-                  widgetIcon.src = "https://media.tenor.com/E3rdAXiaSosAAAAi/cat-heart.gif";
-                  widgetIcon.style.width = "50px"; // Adjust size if needed
-                  widgetIcon.style.height = "50px";
-                  clearInterval(checkExist);
-              }
-          }, 500); // Check every 500ms until the widget loads
-      };
-
       window.chatWidgetScriptLoaded = true;
   })();
 </script>
